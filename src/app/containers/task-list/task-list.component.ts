f7a9bb1dd8ce781ee6d58f4fb71fae1af59f42c0
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-05 15:44:40 +0100

    ref(task-board): make use of task-list

--- src/app/containers/task-list/task-list.component.ts
+++ src/app/containers/task-list/task-list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { Task } from 'src/app/models'
 
 @Component({
   selector: 'tb-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.sass']
 })
-export class TaskListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class TaskListComponent {
+  @Input()
+  tasks: Task[] = []
 }
