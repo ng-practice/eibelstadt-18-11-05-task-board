1552c8e5fe49300e3f0b9e70f1ebbc34ee452d15
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-11-06 10:04:09 +0100

    chore: use PrimeNg

--- src/app/task-board/task-board.module.ts
+++ src/app/task-board/task-board.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { ButtonModule } from 'primeng/button'
+import { CardModule } from 'primeng/card'
 import { AddTaskComponent } from './components/add-task/add-task.component'
 import { TaskCardComponent } from './components/task-card/task-card.component'
 import { TaskBoardComponent } from './containers/task-board/task-board.component'
@@ -12,7 +14,7 @@ import { TaskListComponent } from './containers/task-list/task-list.component'
     TaskCardComponent,
     AddTaskComponent
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, CardModule],
   exports: [TaskBoardComponent]
 })
 export class TaskBoardModule {}
